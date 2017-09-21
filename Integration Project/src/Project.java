61fac233ef3ad1c28fd738ba22688b5d401c254a
cbjonas6063 <cbjonas6063@H339R7S3.STUDENT.ad.fgcu.edu>
2017-09-21 09:43:44 -0400

    Test

---
+++ Integration Project/src/Project.java
@@ -0,0 +1,15 @@
+
+public class Project {
+	//a class is code
+	/**
+	 * Christian Jonas
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		//Single line comment
+		System.out.println("Welcome to my project");
+		
+	}
+
+}
